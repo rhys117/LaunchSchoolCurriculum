b114af18142529795b638b16373f4a136f084201
rhys117 <rhys117@gmail.com>
2016-09-06 21:42:27 +1000

    2140 06Sep2016 21 Basics Complete

--- Backend/101/lesson4/twenty_one.rb
+++ Backend/101/lesson4/twenty_one.rb
@@ -23,7 +23,7 @@ def initialize_deck
   (1..13).each do |num| # spades
    new_deck << [SUITES[3], num]
   end
-  
+
   new_deck
 end
 
@@ -80,20 +80,22 @@ def show_first_card(persons_hand)
   hand << " of #{suit?(persons_hand, 0)}"
 end
 
-def hand_score?(persons_hand, card = 0)
+def hand_score(persons_hand)
   score = 0
-  loop do
-    case persons_hand[card][1]
-    when 2..10
-      score += persons_hand[card][1]
-    when 11..13
+  card_values = persons_hand.map { |card| card[1] }
+  
+  card_values.each do |value|
+    if value == 1
+      score += 11
+    elsif value >= 10
       score += 10
-    when 1
-      score += 1
+    else
+      score += value
     end
+  end
 
-    card += 1
-    break if card == persons_hand.length
+  card_values.select { |card| card == 1 }.count.times do
+    score -= 10 if score > 21
   end
   score
 end
@@ -102,27 +104,94 @@ def hit!(persons_hand, deck)
   persons_hand << rand_card!(deck)
 end
 
-deck = initialize_deck
+def bust?(persons_hand)
+  return false if hand_score(persons_hand) < 22
+  true
+end
 
-dealer_hand = [rand_card!(deck), rand_card!(deck)]
-player_hand = [rand_card!(deck), rand_card!(deck)]
+def winner?(player, dealer)
+  return 'player' if hand_score(player) > hand_score(dealer)
+  return 'draw' if hand_score(player) == hand_score(dealer)
+  return 'dealer'
+end
 
-prompt "Welcome to 21."
-prompt "Dealer hand is #{show_first_card(dealer_hand)} and 'hidden card'"
-puts # line break for readibility
-
-loop do
-  prompt "Your hand is: #{hand_is?(player_hand)}"
-  prompt "Your hand is worth: #{hand_score?(player_hand)}"
-  prompt "Would you like to hit or stay?"
-  hit_or_stay = gets.chomp.downcase
-
-  if hit_or_stay == 'hit'
-    hit!(player_hand, deck)
-  elsif hit_or_stay == 'stay' || bust(player_hand)
-    break
+def print_result(player_hand, dealer_hand)
+  puts "Your score: #{hand_score(player_hand)}; Dealer score: #{hand_score(dealer_hand)}"
+  case winner?(player_hand, dealer_hand)
+  when 'player'
+    prompt "You won!"
+  when 'dealer'
+    prompt "The dealer won!"
   else
-    prompt "sorry I didn't understand that"
+    prompt "It's a draw"
   end
 end
 
+def player_turn!(player_hand, deck)
+  loop do
+    prompt "Your hand is: #{hand_is?(player_hand)}"
+    prompt "Your hand is worth: #{hand_score(player_hand)}"
+    prompt "Would you like to hit or stay?"
+    hit_or_stay = gets.chomp.downcase
+
+    hit!(player_hand, deck) if hit_or_stay == 'hit'
+    break if bust?(player_hand) || hit_or_stay == 'stay'
+  end
+end
+
+def dealer_turn!(dealer_hand, deck)
+  prompt "Dealer hand is #{hand_is?(dealer_hand)}"
+  prompt "Dealers score: #{hand_score(dealer_hand)}"
+  
+  loop do
+    if hand_score(dealer_hand) < 18
+      hit!(dealer_hand, deck)
+      prompt "Dealer Hit!"
+      prompt "Dealer hand is #{hand_is?(dealer_hand)}"
+      prompt "Dealers score: #{hand_score(dealer_hand)}"
+    elsif hand_score(dealer_hand) < 22
+      prompt "Dealer Stays!"
+      break
+    else
+      prompt "Dealer busts! You won!"
+      break
+    end
+  end
+end
+
+
+prompt "Welcome to 21."
+
+loop do
+  deck = initialize_deck
+
+  dealer_hand = [rand_card!(deck), rand_card!(deck)]
+  player_hand = [rand_card!(deck), rand_card!(deck)]
+  
+  loop do
+    prompt "Dealer hand is #{show_first_card(dealer_hand)} and 'hidden card'"
+    puts # line break for readibility
+      
+    player_turn!(player_hand, deck)
+
+    if bust?(player_hand)
+      prompt "Your hand is: #{hand_is?(player_hand)}"
+      prompt "Your hand is worth: #{hand_score(player_hand)}"
+      prompt "You busted! The dealer won" 
+      break   
+    end
+
+    dealer_turn!(dealer_hand, deck)
+      
+    break if bust?(dealer_hand)
+
+    print_result(player_hand, dealer_hand)
+    break
+  end
+
+  prompt "would you like to play again? (y for yes)"
+  play_again = gets.chomp
+  break unless play_again.downcase[0] == 'y'
+end
+
+prompt "Thanks for playing 21!"
