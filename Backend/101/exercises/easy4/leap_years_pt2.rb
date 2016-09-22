76428687b57fc4802c7043404f48a5016d82d9bc
rhys117 <rhys117@gmail.com>
2016-09-22 20:39:35 +1000

    22Sep2016 2040 101 exercises update easy4

--- Backend/101/exercises/easy4/leap_years_pt2.rb
+++ Backend/101/exercises/easy4/leap_years_pt2.rb
@@ -1,7 +1,7 @@
 def leap_year?(year)
   if year % 400 == 0
     true
-  elsif year % 100 == 0
+  elsif year > 1752 && year % 100 == 0
     false
   else
     year % 4 == 0
@@ -17,7 +17,7 @@ puts leap_year?(240001) == false
 puts leap_year?(2000) == true
 puts leap_year?(1900) == false
 puts leap_year?(1752) == true
-puts leap_year?(1700) == false
+puts leap_year?(1700) == true
 puts leap_year?(1) == false
-puts leap_year?(100) == false
+puts leap_year?(100) == true
 puts leap_year?(400) == true
