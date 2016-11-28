86aac5fd014a10bd1d1faf0d7bd3c2d44662ddac
rhys117 <rhys117@gmail.com>
2016-11-28 23:55:24 +1000

    beer song wip

--- Backend/challenges/easy2/beer_song.rb
+++ Backend/challenges/easy2/beer_song.rb
@@ -4,20 +4,37 @@ class BeerSong
     @beers = 99
   end
 
-  def verse(num_of_beers, limit=num_of_beers)
+  def verses(num_of_beers, limit=num_of_beers)
     msg = ''
     until num_of_beers == limit - 1 do
       return last_verse if num_of_beers.zero?
-      msg << "#{num_of_beers} bottles of beer on the wall, #{num_of_beers} bottles of beer.\n" \
-             "Take one down and pass it around, #{num_of_beers - 1} bottles of beer on the wall.\n"
+      msg << verse(num_of_beers)
       num_of_beers -= 1
     end
     msg
   end
-  alias_method :verses, :verse
 
-  def the_whole_song
-    verse(@beers, 0)
+  def bottle_or_bottles(num_of_beers)
+    if num_of_beers > 1
+      return 'bottles'
+    else
+      'bottle'
+    end
+  end
+
+  def verse(num_of_beers)
+    bottles = bottle_or_bottles(num_of_beers)
+    msg = ''
+    msg << "#{num_of_beers} #{bottles} of beer on the wall, #{num_of_beers}"\
+    " #{bottles} of beer.\n"
+    num_of_beers -= 1
+    bottles = bottle_or_bottles(num_of_beers)
+    msg << "Take one down and pass it around, #{num_of_beers - 1} #{bottles} of beer on the wall.\n"
+    msg
+  end
+
+  def lyrics
+    verses(@beers, 0)
   end
 
   def last_verse
