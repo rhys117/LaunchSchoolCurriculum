61ea929f5b0aa10120818f2c618e50e791dd2b88
rhys117 <rhys117@gmail.com>
2016-12-01 17:35:04 +1000

    word count challenge complete

--- Backend/challenges/medium1/phone_number.rb
+++ Backend/challenges/medium1/phone_number.rb
@@ -1,12 +1,25 @@
 class PhoneNumber
+  INVALID_NUMBER = "0000000000".freeze
+
+  attr_reader :number
 
   def initialize(str)
     @input = str
-    @phone_number = convert_input
+    @number = convert_input
   end
 
-  def ten_digit_number
+  def convert_input
+    if valid_number?
+      digit_number
+    else
+      INVALID_NUMBER
+    end
+  end
+
+  def digit_number
     digit_string = @input.gsub(/[^\d]/, '')
+
+    # Remove american code if applicable
     if digit_string.length == 11 && digit_string[0] == '1'
       digit_string[1..-1]
     else
@@ -15,30 +28,14 @@ class PhoneNumber
   end
 
   def valid_number?
-    ten_digit_number.length == 10 && @input.scan(/\p{Alpha}/).empty?
-  end
-
-  def convert_input
-    if valid_number?
-      ten_digit_number
-    else
-      "0000000000"
-    end
-  end
-
-  def number
-    @phone_number
-  end
-
-  def to_s
-    "(#{@phone_number[0..2]}) #{@phone_number[3..5]}-#{@phone_number[6..9]}"
+    digit_number.length == 10 && @input.scan(/\p{Alpha}/).empty?
   end
 
   def area_code
-    "#{@phone_number[0..2]}"
+    "#{@number[0..2]}"
+  end
+
+  def to_s
+    "(#{@number[0..2]}) #{@number[3..5]}-#{@number[6..9]}"
   end
 end
-
-a = PhoneNumber.new('(123) 456-7890')
-p a.valid_number?
-p a.convert_input
