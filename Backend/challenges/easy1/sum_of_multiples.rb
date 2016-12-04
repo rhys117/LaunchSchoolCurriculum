2337c904c57d889257e106a87360ec33c156f78b
rhys117 <rhys117@gmail.com>
2016-12-04 15:40:55 +1000

    sum of multiples WIP

--- Backend/challenges/easy1/sum_of_multiples.rb
+++ Backend/challenges/easy1/sum_of_multiples.rb
@@ -1,28 +1,20 @@
 class SumOfMultiples
   DEFAULT_MULTIPLES = [3, 5]
 
-  initialize(multiples=DEFAULT_MULTIPLES)
+  def initialize(multiples=DEFAULT_MULTIPLES)
     @multiples = *multiples
   end
 
-  def self.to(max, multiples=DEFAULT_MULTIPLES)
-    SumOfMultiples.new(multiples).to(max)
+  def self.to(limit, multiples=DEFAULT_MULTIPLES)
+    result = []
+    multiples.each do |multiple|
+      result << (1..limit).select { |num| num % multiple == 0 }
+    end
+    result.flatten.uniq.inject(:+)
   end
 
   def to(max)
-    self.class.to(max, @multiples)
-  end
-
-  def find_multiples_of_sum
-    result = []
-    @multiples.each do |multiple|
-      result << multiple if num % multiple == 0
-    end
-    result.uniq
-  end
-
-  def find_sum_of_multiples
-    find_multiples_of_sum.inject(:+)
+    self.class.to(limit, @multiples)
   end
 end
 
