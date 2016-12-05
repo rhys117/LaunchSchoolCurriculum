30ae6318bbc26bf79c4d72baf2ff33ed27e0332d
rhys117 <rhys117@gmail.com>
2016-12-06 03:44:50 +1000

    ruby 130 easy1 complete

---
+++ Backend/130/easy1/count_items.rb
@@ -0,0 +1,12 @@
+def count(array)
+  counter = 0
+  array.each { |element| counter += 1 if yield(element) }
+  counter
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
