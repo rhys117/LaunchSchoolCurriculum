74fdd19aace119c8990b86ccd3e4da62e9f676d5
rhys117 <rhys117@gmail.com>
2017-01-06 16:58:14 +1000

    oop notes and code examples updated to closures wip

--- oop_notes_code_examples.rb
+++ oop_notes_code_examples.rb
@@ -2,6 +2,7 @@
   object = "string text"
   object.class # => String
 
+
 # Classes
   class WhatAreClasses
     def initialize
@@ -16,6 +17,7 @@
   object = WhatAreClasses.new
   object.method # => I'm altered data of this object. Hello!
 
+
 # Inheritance
   class HigherInHierachy
     def inherited_method
@@ -28,6 +30,7 @@
   end
 
   LowerInHierachy.new.inherited_method # => I've been inherited!
+
 
 # Super
   class HigherInHierachy
@@ -60,4 +63,119 @@
 
   object = LowerInHierachy.new('first_el', 'second_el') # => #<LowerInHierachy:0x007fdef392bdd0 @first_var="first_el", @second_var="second_el">
 
+
 # Module Methods
+  module AMixIn
+    def a_method
+      "I'm a module method"
+    end
+  end
+
+  class AClass
+    # as CLASS methods
+    extend AMixIn
+
+    # as INSTANCE methods
+    include AMixIn
+  end
+
+
+# Module Namespace
+  module ANamespace
+    class AClass
+      def initialize
+        puts "Another object, coming right up!"
+      end
+    end
+  end
+
+  ANamespace::AClass.new #=> Another object, coming right up!
+
+# Method Lookup
+  module LookedInFirst
+    #
+  end
+
+  module LookedInSecond
+    #
+  end
+
+  class HigherInHierachy
+    include LookedInSecond
+    include LookedInFirst
+    # include LookedInFirst, LookedInSecond
+  end
+
+  class LowerInHierachy < HigherInHierachy
+    #
+  end
+
+  LowerInHierachy.ancestors #=> [LowerInHierachy, HigherInHierachy, LookedInFirst, LookedInSecond, Object, Kernal, BasicObject]
+
+
+# Access Controls
+  class AccessLevel
+    def something_interesting
+      another = AccessLevel.new
+      another.public_method
+      another.protected_method
+      another.private_method
+    end
+
+    def public_method
+      puts "Public method"
+    end
+
+    protected
+
+    def protected_method
+      puts "Protected method"
+    end
+
+    private
+
+    def private_method
+      puts "exception will be raised"
+    end
+  end
+
+  AccessLevel.new.something_interesting
+  #=> Public Method
+  #=> Protected Method
+  #=> NoMethodError: private method
+
+
+# Accessor Methods
+  # Getter :attr_reader
+  def instance_var
+    @instance_var
+  end
+
+  # Setter :attr_writer
+  def instance_var=(value)
+    @instance_var = value
+  end
+
+  # Both :attr_accessor
+
+  # Custom Getter method
+  def instance_var
+    'Mr' + @instance_var.split.last
+  end
+
+# Self
+  class WhatIsSelf
+    def test
+      puts "At the instance level, self is #{self}"
+    end
+
+    def self.test
+      puts "At the class level self is #{self}"
+    end
+  end
+
+  WhatIsSelf.test #=> At the class level self is WhatIsSelf
+
+  WhatIsSelf.new.test #=> At the instance level, self is #<WhatIsSelf:memorylocation>
+
+
