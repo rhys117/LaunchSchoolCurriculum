02d1bf0035615a0aa25c16461163f406bbe6de52
rhys117 <rhys117@gmail.com>
2017-01-11 06:07:05 +1000

    update oop notes and begin house challenge

---
+++ Backend/challenges/wip/medium2/house.rb
@@ -0,0 +1,34 @@
+class House
+
+  def recite
+    result = ''
+    counter = 0
+    until counter == pieces.size
+      result << "This is " + pieces.last
+      counter += 1
+    end
+
+    puts result
+  end
+
+  private
+
+  def pieces
+    [
+      ['the horse and the hound and the horn', 'that belonged to'],
+      ['the farmer sowing his corn', 'that kept'],
+      ['the rooster that crowed in the morn', 'that woke'],
+      ['the priest all shaven and shorn', 'that married'],
+      ['the man all tattered and torn', 'that kissed'],
+      ['the maiden all forlorn', 'that milked'],
+      ['the cow with the crumpled horn', 'that tossed'],
+      ['the dog', 'that worried'],
+      ['the cat', 'that killed'],
+      ['the rat', 'that ate'],
+      ['the malt', 'that lay in'],
+      ['the house that Jack built']
+    ]
+  end
+end
+
+House.new.recite
