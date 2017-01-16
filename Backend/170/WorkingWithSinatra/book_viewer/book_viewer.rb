bf03dc4a2bd376b3c1c28e5f8eeb914103d456f6
rhys117 <rhys117@gmail.com>
2017-01-17 09:24:32 +1000

    Search function added sinatra book viewer

--- Backend/170/WorkingWithSinatra/book_viewer/book_viewer.rb
+++ Backend/170/WorkingWithSinatra/book_viewer/book_viewer.rb
@@ -2,17 +2,60 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |line|
+      "<p>#{line}</p>"
+    end.join
+  end
+end
+
+not_found do
+  redirect "/"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+def each_chapter(&block)
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+   results << {number: number, name: name} if contents.include?(query)
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+
+  erb :search
 end
