4d6b38eb243914f7da9507e7d0976165c0b6a335
rhys117 <rhys117@gmail.com>
2017-01-25 20:37:26 +1000

    170 code challenge complete

--- Backend/170/WorkingWithSinatra/users_and_interests/users_and_interests.rb
+++ Backend/170/WorkingWithSinatra/users_and_interests/users_and_interests.rb
@@ -1,9 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "yaml"
+
+before do
+  @file = YAML.load_file('users.yaml')
+  @users = @file.keys
+end
+
+helpers do
+  def count_interests(users)
+    users.reduce(0) do |sum, (name, user)|
+      sum + user[:interests].size
+    end
+  end
+end
 
 get '/' do
-  @title = "Users and Interests"
+  redirect "/users"
+end
+
+get '/users' do
+  @title = 'Users'
 
   erb :home
+end
+
+get '/users/:id' do
+  @user = params[:id].to_sym
+  @title = @user
+  @email = @file[@user][:email]
+  @interests = @file[@user][:interests]
+
+  erb :user
 end
